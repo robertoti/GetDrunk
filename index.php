73687f04a8090d6664aac18b65d82b9005168231
robertoti <info.robertolima@gmail.com>
2016-03-24 02:02:44 -0300

    Commit do Paulo

--- index.php
+++ index.php
@@ -7,12 +7,16 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>4Drink</title>
     </head>
     <body>
+        <header>
+            
+        </header>
         Nilton clone2
         <?php
-        echo"<br/>"."Commit do paulo1"
+        echo"<br/>"."Commit do paulo1";
+         echo"<br/>"."Commit do paulo2"
         ?>
     </body>
 </html>
